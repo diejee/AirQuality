04e8bd417310f4f206efe748c3c0d5e075e554aa
diejee <jandouweesminia@gmial.com>
2020-04-12 23:26:58 +0200

    app nogwattes

--- frontend/air-quality/src/app/app.module.ts
+++ frontend/air-quality/src/app/app.module.ts
@@ -4,9 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HeaderComponent } from './header/header.component';
+import { RoomlistComponent } from './roomlist/roomlist.component';
+import { RoomComponent } from './roomlist/room/room.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RoomlistComponent,
+    RoomComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
@@ -15,4 +24,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
---
+++ frontend/air-quality/src/app/routing/routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from "@angular/router";
+import {RoomlistComponent} from "../roomlist/roomlist.component";
+import {RoomComponent} from "../roomlist/room/room.component";
+import {PageNotFoundComponent} from "../page-not-found/page-not-found.component";
+
+const appRoutes: Routes = [
+  { path: 'room-list', component: RoomlistComponent },
+  { path: 'room-list/room/:id', component: RoomComponent },
+  { path: '', redirectTo: '/room-list', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule {}
